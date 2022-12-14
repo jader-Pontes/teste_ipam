618936a185b16cd11dabfb1502a1657a8f6809e5
Jader <jaderluiz01@gmail.com>
2022-12-14 16:20:40 -0300

    correção no Home

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Home = () => {
   return (
     <div>
-        <h1>Bem vindo ao Teste do IPHAN</h1>
+        <h1>Bem vindo ao Teste do IPAM</h1>
         <p>Selecione uma das abas do menu para buscar seu Estado ou seu Município</p>
     </div>
   )
